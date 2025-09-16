3da0cd6c3b952772d7cfe73d0cecc27cbf91c7fc
Vijya Laxmi <vijaylaxmi.c1990@gmail.com>
2025-09-16 23:52:52 +0530

    5th Commit

---
+++ src/JavaBasice_02/PrintTable.java
@@ -0,0 +1,14 @@
+package JavaBasice_02;
+
+public class PrintTable {
+    public static void main(String[] args) {
+
+        int n=10;
+        for(int i=1; i<=n; i++)
+        {
+            System.out.printf("9 x %d=%d",i,9*i);
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/JavaBasice_02/PrintTable2.java
@@ -0,0 +1,19 @@
+package JavaBasice_02;
+
+public class PrintTable2 {
+    public static void main(String[] args) {
+        int t =9;
+        System.out.printf("%d x 1=%d%n",t,t*1);
+        System.out.printf("%d x 2=%d%n",t,t*2);
+        System.out.printf("%d x 3=%d%n",t,t*3);
+        System.out.printf("%d x 4=%d%n",t,t*4);
+        System.out.printf("%d x 5=%d%n",t,t*5);
+        System.out.printf("%d x 6=%d%n",t,t*6);
+        System.out.printf("%d x 7=%d%n",t,t*7);
+        System.out.printf("%d x 8=%d%n",t,t*8);
+        System.out.printf("%d x 9=%d%n",t,t*9);
+        System.out.printf("%d x 10=%d%n",t,t*10);
+
+
+    }
+}
